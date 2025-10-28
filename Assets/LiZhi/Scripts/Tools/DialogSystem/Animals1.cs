fd5b9c0f6ab6f9379b6133219a3c34dbe04920ed
lizhisunshine <1390021901@qq.com>
2025-10-28 13:27:10 +0800

    1.01

--- Assets/LiZhi/Scripts/Tools/DialogSystem/Animals1.cs
+++ Assets/LiZhi/Scripts/Tools/DialogSystem/Animals1.cs
@@ -9,7 +9,7 @@ public class Animals1 : AnimalsBase
     public DialogSystem dialogSystem;
     public void Start()
     {
-        TalkNum = 2;
+        TalkNum = 9;
          this.gameObject.GetComponent<Peal>().talkNum=TalkNum;
     }
     public void Update()
@@ -27,20 +27,76 @@ public class Animals1 : AnimalsBase
     public override void Talk()
     {
         switch (TalkNum)
-        { 
+        {
+            case 9:
+                dialogSystem.wordNumber = 0;
+                //dialogSystem.PlayerName = "主角";
+                dialogSystem.ObjName = "村长";
+                dialogSystem.contant = "哇...! 是一个活体狐狸...！你是怎么来到这里的？";
+                dialogSystem.isPlayerTalk = false;
+                dialogSystem.isStart = true;
+                break;
+            case 8:
+                dialogSystem.wordNumber = 0;
+                //dialogSystem.PlayerName = "主角";
+                dialogSystem.ObjName = "村长";
+                dialogSystem.contant = "我也不太清楚，好像刚才我还在上课，一转眼就在这了";
+                dialogSystem.isPlayerTalk = true;
+                dialogSystem.isStart = true;
+                break;
+            case 7:
+                dialogSystem.wordNumber = 0;
+                //dialogSystem.PlayerName = "主角";
+                dialogSystem.ObjName = "村长";
+                dialogSystem.contant = "竟然不明不白来到这么危险的地方了吗";
+                dialogSystem.isPlayerTalk = false;
+                dialogSystem.isStart = true;
+                break;
+            case 6:
+                dialogSystem.wordNumber = 0;
+                //dialogSystem.PlayerName = "主角";
+                dialogSystem.ObjName = "村长";
+                dialogSystem.contant = "这里最近很不太平，我建议你还是离开这个地方";
+                dialogSystem.isPlayerTalk = false;
+                dialogSystem.isStart = true;
+                break;
+            case 5:
+                dialogSystem.wordNumber = 0;
+                //dialogSystem.PlayerName = "主角";
+                dialogSystem.ObjName = "村长";
+                dialogSystem.contant = "您说的有道理，这看着就不太安全。我应该怎样才能离开？";
+                dialogSystem.isPlayerTalk = true;
+                dialogSystem.isStart = true;
+                break;
+            case 4:
+                dialogSystem.wordNumber = 0;
+                //dialogSystem.PlayerName = "主角";
+                dialogSystem.ObjName = "村长";
+                dialogSystem.contant = "去找除我之外的五只小动物，它们被困在这个森林各处的灯笼里面了";
+                dialogSystem.isPlayerTalk = false;
+                dialogSystem.isStart = true;
+                break;
+            case 3:
+                dialogSystem.wordNumber = 0;
+                //dialogSystem.PlayerName = "主角";
+                dialogSystem.ObjName = "村长";
+                dialogSystem.contant = "找到它们，然后回到这里，我们就能帮你回家。";
+                dialogSystem.isPlayerTalk = false;
+                dialogSystem.isStart = true;
+                break;
             case 2:
                 dialogSystem.wordNumber = 0;
                 //dialogSystem.PlayerName = "主角";
                 dialogSystem.ObjName = "村长";
-                dialogSystem.contant = "哇...! 是一个活体狐狸...！";
-                dialogSystem.isPlayerTalk = false;
+                dialogSystem.contant = "感谢你能跟我说这些";
+                dialogSystem.isPlayerTalk = true;
                 dialogSystem.isStart = true;
                 break;
             case 1:
                 dialogSystem.wordNumber = 0;
                 //dialogSystem.PlayerName = "主角";
                 dialogSystem.ObjName = "村长";
-                dialogSystem.contant = "我认为紧跟着飞蛾是一个好选择";
+                dialogSystem.contant = "不用谢，如果你不知道该去哪里了，就跟随飞虫的脚步，这些小东西会给你指明方向。";
                 dialogSystem.isPlayerTalk = false;
                 dialogSystem.isStart = true;
                 break;
@@ -48,7 +104,7 @@ public class Animals1 : AnimalsBase
                 dialogSystem.wordNumber = 0;
                 //dialogSystem.PlayerName = "主角";
                 dialogSystem.ObjName = "村长";
-                dialogSystem.contant = "我听说用”R“可以回到篝火前...";
+                dialogSystem.contant = "对了，这个森林有很多非常神奇的物品，记得多按e和它们互动，这对你的旅途会有很大帮助。";
                 dialogSystem.isPlayerTalk = false;
                 dialogSystem.isStart = true;
                 break;
